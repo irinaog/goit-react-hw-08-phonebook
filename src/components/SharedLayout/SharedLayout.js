0bfab00e79ea705e1d98fcb2ea31b526442bf0f9
irinaog <goncharirina08@gmail.com>
2022-08-29 14:43:04 +0300

    Update SharedLayout.js

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -3,18 +3,18 @@ import { Outlet } from "react-router-dom";
 import { AppBar } from "components/AppBar";
 import { Loader } from "components/Loader/Loader";
 import Container from '@mui/material/Container';
-// import authSelectors from "auth/auth-selectors";
-// import { useSelector } from "react-redux";
+import authSelectors from "auth/auth-selectors";
+import { useSelector } from "react-redux";
 
 
 export const SharedLayout = () => {
-  // const isLogged = useSelector(authSelectors.getIsLoggedIn);
+  const isLogged = useSelector(authSelectors.getIsLoggedIn);
 
     return (
     <>
       <AppBar/>
         <Container maxWidth='lg'>
-          {/* {!isLogged && <div style={{
+          {!isLogged && <div style={{
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -24,7 +24,7 @@ export const SharedLayout = () => {
     fontWeight: 500,
     fontSize: '32px',
     lineHeight: 1.75,
-          }}>Please login or register </div>} */}
+          }}>Please login or register </div>}
         <Suspense fallback={<Loader/>}>
             <Outlet />
         </Suspense>
